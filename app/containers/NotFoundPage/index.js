c61f91a99bad8fd29423adc13a9b7f43b1d297e8
Balint Soos <soosbalint95@gmail.com>
2017-09-30 17:55:40 +0200

    NotFoundPage with Helmet wrapper

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -11,15 +11,20 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import Head from 'components/Head';
 
 import messages from './messages';
 
 export default class NotFound extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <Head title={messages.title} />
+
+        <h1>
+          <FormattedMessage {...messages.title} />
+        </h1>
+      </div>
     );
   }
 }
--- app/containers/NotFoundPage/messages.js
+++ app/containers/NotFoundPage/messages.js
@@ -6,8 +6,8 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  header: {
-    id: 'app.components.NotFoundPage.header',
-    defaultMessage: 'This is NotFoundPage component!',
+  title: {
+    id: 'app.components.NotFoundPage.title',
+    defaultMessage: '404',
   },
 });
