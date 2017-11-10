85805a46c924945aa75af48864aef3debd54fa86
Balint Soos <soosbalint95@gmail.com>
2017-11-10 18:15:41 +0100

    muiThemeable Logo

--- app/components/Logo/index.js
+++ app/components/Logo/index.js
@@ -1,22 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { Flex } from 'grid-styled';
-import { cyan500 } from 'material-ui/styles/colors';
+import muiThemeable from 'material-ui/styles/muiThemeable';
 
 const Text = styled(Link)`
   font-size: 40px;
   font-weight: 300;
-  color: ${cyan500};
+  color: ${(props) => props.color};
   text-decoration: none;
+  transition: all 0.2s;
+
+  &:hover {
+    color: ${(props) => props.hover}
+  }
 `;
 
-const Logo = () => (
+const Logo = (props) => (
   <Flex justify={'center'}>
-    <Text to={'/'}>
+    <Text
+      to={'/'}
+      color={props.muiTheme.palette.primary1Color}
+      hover={props.muiTheme.palette.accent4Color}
+    >
       {'rezsi.io'}
     </Text>
   </Flex>
 );
 
-export default Logo;
+Logo.propTypes = {
+  muiTheme: PropTypes.object.isRequired,
+};
+
+export default muiThemeable()(Logo);
--- app/utils/muiTheme.js
+++ app/utils/muiTheme.js
@@ -1,11 +1,14 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-// Colors
-import { grey50 as accent1Color } from 'material-ui/styles/colors';
+import {
+  grey50 as accent1Color,
+  cyan200 as accent4Color,
+} from 'material-ui/styles/colors';
 
 const muiTheme = getMuiTheme({
   palette: {
     accent1Color,
+    accent4Color,
   },
 });
 
