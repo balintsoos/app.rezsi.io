15981d3eb85c7c7c7370f743c270cfd25d7687a1
Balint Soos <soosbalint95@gmail.com>
2017-11-11 20:00:54 +0100

    Logo warning fixed

--- app/components/Logo/index.js
+++ app/components/Logo/index.js
@@ -1,36 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { Flex } from 'grid-styled';
 import muiThemeable from 'material-ui/styles/muiThemeable';
 
-const Text = styled(Link)`
+const Text = muiThemeable()(styled(({ muiTheme, ...rest }) => <Link {...rest} />)`
   font-size: 40px;
   font-weight: 300;
-  color: ${(props) => props.color};
+  color: ${(props) => props.muiTheme.palette.primary1Color};
   text-decoration: none;
   transition: all 0.2s;
 
   &:hover {
-    color: ${(props) => props.hover}
+    color: ${(props) => props.muiTheme.palette.accent4Color};
   }
-`;
+`);
 
-const Logo = (props) => (
+const Logo = () => (
   <Flex justify={'center'}>
-    <Text
-      to={'/'}
-      color={props.muiTheme.palette.primary1Color}
-      hover={props.muiTheme.palette.accent4Color}
-    >
+    <Text to={'/'}>
       {'rezsi.io'}
     </Text>
   </Flex>
 );
 
-Logo.propTypes = {
-  muiTheme: PropTypes.object.isRequired,
-};
-
-export default muiThemeable()(Logo);
+export default Logo;
